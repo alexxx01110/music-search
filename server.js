748d245c65ec0fbc65da257ebdbab064acc95504
Alex_m <kingkong2008@ukr.net>
2018-04-21 12:12:36 +0300

    Add server.js config to deploy app on Heroku. Add express support to package.json.

---
+++ server.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var path = require('path');
+
+var server_port = process.env.PORT || 5000;
+
+// var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+//
+// var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+
+app = express();
+
+app.use('/dist', express.static('dist'));
+
+console.log('dir name = ', __dirname)
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, './index.html'))
+});
+
+// app.listen(server_port, server_ip_address, function () {
+//   console.log('Listening on ' + server_ip_address + ', port ' + server_port)
+// });
+app.listen(server_port, function () {
+  console.log('port: ' + server_port)
+});
